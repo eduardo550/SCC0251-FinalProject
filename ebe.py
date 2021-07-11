5c9c1cf163a1f8c83a96975bc23c39fc4b6809ee
Igor Grécia <igorgrecia@gmail.com>
2021-07-11 18:16:48 -0300

    Add files via upload

--- ebe.py
+++ ebe.py
@@ -5,6 +5,7 @@ import sys
 import cv2
 import numpy as np
 import lsb
+import math
 
 class SteganographyException(Exception):
     pass
@@ -12,6 +13,7 @@ class SteganographyException(Exception):
 class Steg():
     def __init__(self, im):
         self.image = im
+        self.sobel_x, self.sobel_y = ebe_get_sobel(im)
         self.height, self.width, self.nbchannels = im.shape
         self.size = self.width * self.height
         
@@ -46,8 +48,10 @@ class Steg():
         else:
             self.curchan +=1
 
-    def lsb_put_binary_value(self, bits): #Put the bits in the image
+    def ebe_put_binary_value(self, bits): #Put the bits in the image
         for c in bits:
+            while self.ebe_gradient() < 50:
+                self.next_slot()
             val = list(self.image[self.curheight,self.curwidth]) #Get the pixel value as a list
             if int(c) == 1:
                 val[self.curchan] = int(val[self.curchan]) | self.maskONE #OR with maskONE
@@ -58,7 +62,14 @@ class Steg():
             self.next_slot() #Move "cursor" to the next space
         return
 
+    def ebe_gradient(self):
+        x = self.sobel_x[self.curheight,self.curwidth]
+        y = self.sobel_y[self.curheight,self.curwidth]
+        return math.sqrt(x**2 + y**2)
+
     def read_bit(self): #Read a single bit into the image
+        while self.ebe_gradient() < 50:
+            self.next_slot()
         val = self.image[self.curheight,self.curwidth][self.curchan]
         val = int(val) & self.maskONE
         self.next_slot()
@@ -86,64 +97,15 @@ class Steg():
         while len(binval) < bitsize:
             binval = "0"+binval
         return binval
-
-    def encode_text(self, txt):
-        l = len(txt)
-        binl = self.binary_value(l, 16) #Length coded on 2 bytes so the text size can be up to 65536 bytes long
-        self.lsb_put_binary_value(binl) #Put text length coded on 4 bytes
-        for char in txt: #And put all the chars
-            c = ord(char)
-            self.lsb_put_binary_value(self.byteValue(c))
-        return self.image
-       
-    def decode_text(self):
-        ls = self.read_bits(16) #Read the text size in bytes
-        l = int(ls,2)
-        i = 0
-        unhideTxt = ""
-        while i < l: #Read all bytes of the text
-            tmp = self.read_byte() #So one byte
-            i += 1
-            unhideTxt += chr(int(tmp,2)) #Every chars concatenated to str
-        return unhideTxt
-
-    def encode_image(self, imtohide):
-        w = imtohide.width
-        h = imtohide.height
-        if self.width*self.height*self.nbchannels < w*h*imtohide.channels:
-            raise SteganographyException("Carrier image not big enough to hold all the datas to steganography")
-        binw = self.binary_value(w, 16) #Width coded on to byte so width up to 65536
-        binh = self.binary_value(h, 16)
-        self.lsb_put_binary_value(binw) #Put width
-        self.lsb_put_binary_value(binh) #Put height
-        for h in range(imtohide.height): #Iterate the hole image to put every pixel values
-            for w in range(imtohide.width):
-                for chan in range(imtohide.channels):
-                    val = imtohide[h,w][chan]
-                    self.lsb_put_binary_value(self.byteValue(int(val)))
-        return self.image
-
-                    
-    def decode_image(self):
-        width = int(self.read_bits(16),2) #Read 16bits and convert it in int
-        height = int(self.read_bits(16),2)
-        unhideimg = np.zeros((width,height, 3), np.uint8) #Create an image in which we will put all the pixels read
-        for h in range(height):
-            for w in range(width):
-                for chan in range(unhideimg.channels):
-                    val = list(unhideimg[h,w])
-                    val[chan] = int(self.read_byte(),2) #Read the value
-                    unhideimg[h,w] = tuple(val)
-        return unhideimg
     
     def encode_binary(self, data):
         l = len(data)
         if self.width*self.height*self.nbchannels < l+64:
             raise SteganographyException("Carrier image not big enough to hold all the datas to steganography")
-        self.lsb_put_binary_value(self.binary_value(l, 64))
+        self.ebe_put_binary_value(self.binary_value(l, 64))
         for byte in data:
             byte = byte if isinstance(byte, int) else ord(byte) # Compat py2/py3
-            self.lsb_put_binary_value(self.byteValue(byte))
+            self.ebe_put_binary_value(self.byteValue(byte))
         return self.image
 
     def decode_binary(self):
@@ -153,6 +115,11 @@ class Steg():
             output += bytearray([int(self.read_byte(),2)])
         return output
 
+def ebe_get_sobel(img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        sobel_x = cv2.Sobel(gray, cv2.CV_16S, 1, 0, ksize=3, scale=1, delta=0, borderType=cv2.BORDER_DEFAULT)
+        sobel_y = cv2.Sobel(gray, cv2.CV_16S, 0, 1, ksize=3, scale=1, delta=0, borderType=cv2.BORDER_DEFAULT)
+        return sobel_x, sobel_y
 
 def main(user_opt, image_name, payload):
     in_img = cv2.imread(image_name, cv2.IMREAD_COLOR)
@@ -161,6 +128,10 @@ def main(user_opt, image_name, payload):
     if (user_opt == 'encode'):
         with open(payload, 'rb') as f:
             data = f.read()
+        with open("sobelx.txt", "wb") as f:
+            f.write(steg.sobel_x)
+        with open("sobely.txt", "wb") as f:
+            f.write(steg.sobel_y)
         res = steg.encode_binary(data)
         filename = input("Enter new image name(without extension): ")
         file = filename + ".png"
@@ -169,7 +140,7 @@ def main(user_opt, image_name, payload):
     elif (user_opt == 'decode'):
         raw = steg.decode_binary()
         print("Steg is:")
-        print(raw)
+        #print(raw)
         with open(payload, "wb") as f:
             f.write(raw)
 
